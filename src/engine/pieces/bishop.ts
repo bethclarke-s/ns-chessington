252205da310dd39db82d3181d83ba9bb74663b43
bethclarke-s <beth.clarke@softwire.com>
2026-09-10 17:25:53 +0100

    Fourth test passing

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,34 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        const currentRow = currentSquare.row;
+        const currentCol = currentSquare.col;
+
+        const availableMoves = [];
+        
+        for (let i=1; i < GameSettings.BOARD_SIZE; i++){
+
+            if (currentRow + i < GameSettings.BOARD_SIZE && currentCol + i < GameSettings.BOARD_SIZE){
+                availableMoves.push(Square.at(currentRow + i, currentCol + i));
+            } 
+            
+            if (currentRow - i >= 0 && currentCol - i >= 0){
+                availableMoves.push(Square.at(currentRow - i, currentCol - i));
+            }
+
+            if (currentRow + i < GameSettings.BOARD_SIZE && currentCol - i >= 0){
+                availableMoves.push(Square.at(currentRow + i, currentCol - i));
+            } 
+            
+            if (currentRow - i >= 0 && currentCol + i < GameSettings.BOARD_SIZE){
+                availableMoves.push(Square.at(currentRow - i, currentCol + i));
+            }
+
+        }
+
+        console.log(availableMoves);
+        return availableMoves;
     }
 }
