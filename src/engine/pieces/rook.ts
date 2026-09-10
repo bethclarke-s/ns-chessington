255a8e15e78536280348382838f2fdcf746884f0
bethclarke-s <beth.clarke@softwire.com>
2026-09-10 15:00:53 +0100

    Third test passing

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,30 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        const currentRow = currentSquare.row;
+        const currentCol = currentSquare.col;
+
+        const availableMoves = [];
+        
+        for (let i=0; i < GameSettings.BOARD_SIZE; i++){
+
+            if (i != currentCol){
+
+                availableMoves.push(Square.at(currentRow,i));
+
+            }
+        }
+        for (let i=0; i < GameSettings.BOARD_SIZE; i++){
+            
+            if (i != currentRow){
+
+                availableMoves.push(Square.at(i,currentCol));
+
+            }
+        }
+
+        return availableMoves;
     }
 }
