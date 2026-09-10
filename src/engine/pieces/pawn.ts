cf2a3cf99f9d602d4f50f8241ff79a1bd693a507
bethclarke-s <beth.clarke@softwire.com>
2026-09-10 09:34:56 +0100

    First test passing

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        const availableMoves = [];
+        if (this.player === Player.WHITE) {
+            availableMoves.push(Square.at(currentSquare.row + 1, currentSquare.col));
+        } else {
+            availableMoves.push(Square.at(currentSquare.row - 1, currentSquare.col))
+        }
+
+        return availableMoves;
     }
 }
