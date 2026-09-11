0917fafd94fb3eb6c0a1866a2d7447184bbc00bd
bethclarke-s <beth.clarke@softwire.com>
2026-09-11 12:18:19 +0100

    Fifth test passing (using AI)

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,47 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        const currentRow = currentSquare.row;
+        const currentCol = currentSquare.col;
+
+        const availableMoves = [];
+        
+        const boardSize = GameSettings.BOARD_SIZE;
+        
+        // Lateral moves (like Rook)
+        for (let i = 0; i < boardSize; i++) {
+            if (i != currentCol) {
+                availableMoves.push(Square.at(currentRow, i));
+            }
+            
+            if (i != currentRow) {
+                availableMoves.push(Square.at(i, currentCol));
+            }
+        }
+        
+        // Diagonal moves (like Bishop)
+        for (let i = 1; i < boardSize; i++) {
+            // Forwards diagonal
+            if (currentRow + i < boardSize && currentCol + i < boardSize) {
+                availableMoves.push(Square.at(currentRow + i, currentCol + i));
+            }
+            
+            if (currentRow - i >= 0 && currentCol - i >= 0) {
+                availableMoves.push(Square.at(currentRow - i, currentCol - i));
+            }
+
+            // Backwards diagonal
+            if (currentRow + i < boardSize && currentCol - i >= 0) {
+                availableMoves.push(Square.at(currentRow + i, currentCol - i));
+            }
+            
+            if (currentRow - i >= 0 && currentCol + i < boardSize) {
+                availableMoves.push(Square.at(currentRow - i, currentCol + i));
+            }
+        }
+
+        return availableMoves;
     }
 }
