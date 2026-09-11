86f162a09e052763b51e2522f3268e3d9d488971
bethclarke-s <beth.clarke@softwire.com>
2026-09-11 15:06:33 +0100

    Sixth test passing

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,26 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const { row, col } = currentSquare;
+        const boardSize = GameSettings.BOARD_SIZE;
+
+        const availableMoves = [];
+        
+        const knightOffsets = [
+            [2, 1], [2, -1], [-2, 1], [-2, -1],  // 2 squares vertical, 1 square horizontal
+            [1, 2], [1, -2], [-1, 2], [-1, -2]   // 1 square vertical, 2 squares horizontal
+        ];
+
+        for (const [rowOffset, colOffset] of knightOffsets) {
+            const newRow = row + rowOffset;
+            const newCol = col + colOffset;
+
+            if (newRow >= 0 && newRow < boardSize && newCol >= 0 && newCol < boardSize) {
+                availableMoves.push(Square.at(newRow, newCol));
+            }
+        }
+
+        return availableMoves;
     }
 }
